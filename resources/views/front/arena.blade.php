1132d303b4185b595a4a60fa414edb57997c375b
Maulik Shah <maulik.shah1910@gmail.com>
2025-08-22 02:49:49 +0530

    Added: Routing for code arena pages for front-end

--- resources/views/front/arena.blade.php
+++ resources/views/front/arena.blade.php
@@ -5,8 +5,69 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Practice Arena</title>
+
+    <link rel="stylesheet" href="{!! asset('front/css/bootstrap.min.css') !!}" />
+    <link rel="stylesheet" href="{!! asset('front/css/bootstrap-grid.min.css') !!}" />
+
 </head>
 <body>
-    <h1> Let's Setup the practice arena here... </h1>
+
+    <div class="container-fluid vh-100">
+        <div class="row h-100">
+            <div class="col-md-6 border-end p-3 overflow-auto">
+                <h1> Your problem statement shall appear here... </h1>
+                
+            </div>
+            
+            <!-- Right: Code Editor + Results -->
+            <div class="col-md-6 d-flex flex-column p-0">
+                <!-- Editor Section -->
+                <div class="p-3 border-bottom flex-grow-1">
+                    <div class="d-flex align-items-center mb-2">
+                        <label for="language" class="me-2 fw-bold">Language:</label>
+                        <select name="language" id="language" class="form-select w-auto">
+                            <option value="python">Python</option>
+                            <option value="cpp">C++</option>
+                            <option value="java">Java</option>
+                            <option value="sql">SQL</option>
+                            <option value="php">PHP</option>
+                        </select>
+                        <button id="runBtn" class="btn btn-primary ms-3">Run</button>
+                    </div>
+
+                    <!-- Code editor placeholder -->
+                    <div id="editor" class="border rounded bg-light" style="height:300px;"></div>
+                </div>
+
+                <!-- Results Section -->
+                <div class="p-3 flex-grow-1 overflow-auto">
+                    <h5>Results</h5>
+                    <div id="results" class="border rounded p-2 bg-light" style="min-height:100px;">
+                    <!-- Execution results will be displayed here -->
+                    </div>
+
+                </div>
+                
+                <!-- Custom Test Cases -->
+                <div class="mt-4">
+                    <h6>Add Custom Test Case</h6>
+                    <div class="mb-2">
+                        <textarea id="customInput" class="form-control" rows="2" placeholder="Input"></textarea>
+                    </div>
+                    <div class="mb-2">
+                        <textarea id="customOutput" class="form-control" rows="2" placeholder="Expected Output"></textarea>
+                    </div>
+                    <button id="customBtn" class="btn btn-success">Run Custom</button>
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
+
+
+    <script src="{!! asset('front/js/bootstrap.min.js') !!}" type="javascript"></script>
+    <script src="{!! asset('front/js/bootstrap.bundle.min.js') !!}" type="javascript"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Front\ArenaController;
+use App\Http\Controllers\Front\ChallengeController;
+use App\Http\Controllers\Front\SkillController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +19,18 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::prefix('skills')
+    ->group(function() {
+        Route::get('/', [SkillController::class, 'skills'])->name('skills');
+        Route::get('/{slug}', [SkillController::class, 'getSkillQuestions'])->name('skills.single');
+    });
+
+Route::prefix('challenges')
+    ->group(function() {
+        Route::get('/', [SkillController::class, 'skills'])->name('challenge');
+        Route::get('/{slug}', [ChallengeController::class, 'getSingleChallenge'])->name('challaenge.single');
+    });
 
 Route::get('/arena', [ArenaController::class, 'index'])->name('arena');
 
